16f59cdf9004f736c5b6eac422a3002264059e31
Margaret Meehan <mameehan@microsoft.com>
2017-09-14 18:19:04 -0400

    some comments and cleaning up

--- Xamarin Android/Xamarin_Android.Android/VideoStream.cs
+++ Xamarin Android/Xamarin_Android.Android/VideoStream.cs
@@ -16,23 +16,30 @@ using Java.Lang;
 using Android.Graphics;
 using Android.Hardware.Display;
 using Android.Media.Projection;
-using Android.Graphics;
 
 namespace Xamarin_Android.Droid
 {
     [Activity(Label = "VideoStream")]
-    public class VideoStream : Activity, PeerConnection.IObserver, ISdpObserver// //IVideoCapturer
+    public class VideoStream : Activity, PeerConnection.IObserver, ISdpObserver //HEATHER: IVideoCapturer
     {
 
-        public ISdpObserver sdp;
+        // Observes SDP-related events
+        public ISdpObserver sdpObserver;
 
         public PeerConnection.IObserver observer { get; set; }
 
-        public IVideoCapturer videoCapturer { get; set; }
+        //public IVideoCapturer videoCapturer { get; set; }
+
+        private VideoRenderer.ICallbacks remoteRender;
+        private VideoRenderer.ICallbacks localRender;
+        Org.Webrtc.SurfaceViewRenderer remoteRenderView;
+        Org.Webrtc.SurfaceViewRenderer localRenderView;
+        private VideoTrack remoteVideoTrack;
+        private EglBase rootEglBase;
+
 
         /* HEATHER: public ISdpObserver sdp { get; set; }
-
-        
+         * 
         private IVideoCapturerCapturerObserver capturerObserver { get; set; }
         private SurfaceTextureHelper surfaceTextureHelper { get; set; }
        
@@ -43,19 +50,9 @@ namespace Xamarin_Android.Droid
         private MediaProjectionManager mediaProjectionManager;
         private Intent mediaProjectionPermissionResultData;
         private MediaProjection.Callback mediaProjectionCallback;
-        
         */
 
-        private VideoRenderer.ICallbacks remoteRender;
-        private VideoRenderer.ICallbacks localRender;
-        Org.Webrtc.SurfaceViewRenderer remoteRenderView;
-        Org.Webrtc.SurfaceViewRenderer localRenderView;
-        private VideoTrack remoteVideoTrack;
-
-        private EglBase rootEglBase;
-
         /* HEATHER 
-        
         private void checkNotDisposed()
         {
             if (isDisposed)
@@ -70,13 +67,13 @@ namespace Xamarin_Android.Droid
             {
                 return true;
             }
-        }*/
+        }
+        */
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            //rootEglBase = EglBase.Create();
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.VideoStream);
             remoteRenderView = FindViewById<Org.Webrtc.SurfaceViewRenderer> (Resource.Id.remote_video_view);
@@ -87,40 +84,34 @@ namespace Xamarin_Android.Droid
             remoteRenderView.SetEnableHardwareScaler(true);
             updateVideoView();
 
-            PeerConnectionFactory.InitializeAndroidGlobals(this, true, false, false);
             string vstream = Intent.GetStringExtra("video_stream");
 
             PeerConnection.IceServer ice = new PeerConnection.IceServer("turnserver3dstreaming.centralus.cloudapp.azure.com:5349", "user", "3Dtoolkit072017");
             IList<PeerConnection.IceServer> servers = new List<PeerConnection.IceServer>();
-
-            //SHOULDNT we also have a stun server? 
+            //q: shouldn't we also have a stun server? 
             servers.Add(ice);
 
+            /* Handles the creating of audio and video streams. */
+            PeerConnectionFactory.InitializeAndroidGlobals(this, true, false, false);
             PeerConnectionFactory pcFactory = new PeerConnectionFactory();
 
-            // First we create an AudioSource then we can create our AudioTrack
+            /* Set initial audio and video constraints */
             MediaConstraints audioConstraints = new MediaConstraints();
             MediaConstraints sdpConstraints = new MediaConstraints();
             sdpConstraints.Mandatory.Add(new MediaConstraints.KeyValuePair("OfferToReceiveAudio", "true"));
             sdpConstraints.Mandatory.Add(new MediaConstraints.KeyValuePair("OfferToReceiveVideo", "true"));
+
+            /* Create the local VideoCapturer (for now) and tracks, of front facing camera and audio.*/
+            IVideoCapturer videoCapturer = createVideoCapturer();
+            VideoSource videoSource = pcFactory.CreateVideoSource(videoCapturer);
+            videoCapturer.StartCapture(100, 100, 30); //change these constants
+            VideoTrack localVideoTrack = pcFactory.CreateVideoTrack("vidtrack", videoSource);
+            // localVideoTrack.AddRenderer(new VideoRenderer(localRender));
+
             AudioSource audioSource = pcFactory.CreateAudioSource(audioConstraints);
             AudioTrack localAudioTrack = pcFactory.CreateAudioTrack("sad", audioSource);
-
-            //CREATE LOCAL VIDEO TRACK
-
-            videoCapturer = createVideoCapturer();
-
-            // HEATHER surfaceTextureHelper = SurfaceTextureHelper.Create("current", new EglBaseContext());            
-            // HEATHER videoCapturer.Initialize(surfaceTextureHelper,this, capturerObserver);
-
-            VideoSource videoSource = pcFactory.CreateVideoSource(videoCapturer);
-            //change these constants
-            videoCapturer.StartCapture(100, 100, 30);
-            VideoTrack localVideoTrack = pcFactory.CreateVideoTrack("vidtrack", videoSource);
-
-
-            // We start out with an empty MediaStream object, created with help from our PeerConnectionFactory
-            //  Note that LOCAL_MEDIA_STREAM_ID can be any string
+            
+            /* Add local stracks to the Media Stream. This is how local audio/video displays on your phone*/
             MediaStream mediaStream = pcFactory.CreateLocalMediaStream("heather");
             mediaStream.AddTrack(localAudioTrack);
             mediaStream.AddTrack(localVideoTrack);
@@ -130,13 +121,14 @@ namespace Xamarin_Android.Droid
             peerConnection.AddStream(mediaStream);
 
             
-            peerConnection.CreateOffer(sdp, sdpConstraints);
-            peerConnection.CreateAnswer(sdp, audioConstraints);
+            peerConnection.CreateOffer(sdpObserver, sdpConstraints);
+            peerConnection.CreateAnswer(sdpObserver, audioConstraints);
 
-            //// var uri = Android.Net.Uri.Parse("http://ia600507.us.archive.org/25/items/Cartoontheater1930sAnd1950s1/PigsInAPolka1943.mp4");
+            // var uri = Android.Net.Uri.Parse("http://ia600507.us.archive.org/25/items/Cartoontheater1930sAnd1950s1/PigsInAPolka1943.mp4");
 
+            // HEATHER surfaceTextureHelper = SurfaceTextureHelper.Create("current", new EglBaseContext());            
+            // HEATHER videoCapturer.Initialize(surfaceTextureHelper,this, capturerObserver);
             
-            //localVideoTrack.AddRenderer(new VideoRenderer(localRender));
             //videoView.SetVideoURI(Android.Net.Uri.Parse(ice.Uri));
             //videoView.Visibility = ViewStates.Visible;
             //videoView.Start();
@@ -222,96 +214,24 @@ namespace Xamarin_Android.Droid
             throw new NotImplementedException();
         }
 
-        /* public void Initialize(SurfaceTextureHelper surfaceTextureHelper, Context applicationContext, IVideoCapturerCapturerObserver capturerObserver)
-        {
-            if (capturerObserver == null)
-            {
-                throw new RuntimeException("capturerObserver not set.");
-            }
-            this.capturerObserver = capturerObserver;
-            if (surfaceTextureHelper == null)
-            {
-                throw new RuntimeException("surfaceTextureHelper not set.");
-            }
-            this.surfaceTextureHelper = surfaceTextureHelper;
-            mediaProjectionManager = (MediaProjectionManager)applicationContext.GetSystemService(
-                Context.MediaProjectionService);
-        }
-
-        public void StartCapture(int width, int height, int ignoredFramerate)
-        {
-
-            this.width = width;
-            this.height = height;
-            mediaProjection = mediaProjectionManager.GetMediaProjection(
-                1, mediaProjectionPermissionResultData);
-            //// Let MediaProjection callback use the SurfaceTextureHelper thread.
-            //mediaProjection.RegisterCallback(mediaProjectionCallback, surfaceTextureHelper.getHandler());
-            //createVirtualDisplay();
-            //capturerObserver.OnCapturerStarted(true);
-            //surfaceTextureHelper.StartListening();
-        }
-        private void createVirtualDisplay()
-        {
-            //surfaceTextureHelper.getSurfaceTexture().setDefaultBufferSize(width, height);
-            //virtualDisplay = mediaProjection.createVirtualDisplay("WebRTC_ScreenCapture", width, height,
-            //    VIRTUAL_DISPLAY_DPI, DISPLAY_FLAGS, new Surface(surfaceTextureHelper.getSurfaceTexture()),
-            //    null, null callback handler);
-        }
-        public void StopCapture()
-        {
-            throw new NotImplementedException();
-        }
-        */
-
-        /* public IVideoCapturer getVideoCapturer()
-        {
-
-            string[] cameraFacing = { "front", "back" };
-            int[] cameraIndex = { 0, 1 };
-            int[] cameraOrientation = { 0, 90, 180, 270 };
-            foreach (string facing in cameraFacing)
-            {
-                foreach (int index in cameraIndex)
-                {
-                    foreach (int orientation in cameraOrientation)
-                    {
-                        string name = "Camera " + index + ", Facing " + facing +
-                            ", Orientation " + orientation;
-                        System.Diagnostics.Debug.Print(name);
-                        IVideoCapturer capturer = Create(name);
-
-                        if (capturer != null)
-                        {
-                            Console.Write("Using camera: " + name);
-                            return capturer;
-                        }
-                    }
-                }
-            }
-            throw new RuntimeException("Failed to open capturer");
-
-
-            } */
-
-        private IVideoCapturer createVideoCapturer()
-       
         //private IVideoCapturer getVideoCapturer()
+        private IVideoCapturer createVideoCapturer()
         {
-            IVideoCapturer videoCapturer;
+            IVideoCapturer capturer;
+
             if (useCamera2())
             {
-                videoCapturer = createCameraCapturer(new Camera2Enumerator(this));
+                capturer = createCameraCapturer(new Camera2Enumerator(this));
             }
             else
             {
-                videoCapturer = createCameraCapturer(new Camera1Enumerator(true));
+                capturer = createCameraCapturer(new Camera1Enumerator(true));
             }
-            if (videoCapturer == null)
+            if (capturer == null)
             {
                 return null;
             }
-            return videoCapturer;
+            return capturer;
         }
 
         private IVideoCapturer createCameraCapturer(ICameraEnumerator enumerator)
@@ -324,11 +244,11 @@ namespace Xamarin_Android.Droid
                 if (enumerator.IsFrontFacing(deviceName))
                 {
                     //Logging.d(LOG_TAG, "Creating front facing camera capturer.");
-                    ICameraVideoCapturer videoCapturer = enumerator.CreateCapturer(deviceName, null);
+                    IVideoCapturer capturer = enumerator.CreateCapturer(deviceName, null);
 
-                    if (videoCapturer != null)
+                    if (capturer != null)
                     {
-                        return videoCapturer;
+                        return capturer;
                     }
                 }
             }
@@ -338,12 +258,11 @@ namespace Xamarin_Android.Droid
             {
                 if (!enumerator.IsFrontFacing(deviceName))
                 {
-                    //Logging.d(LOG_TAG, "Creating other camera capturer.");
-                    IVideoCapturer videoCapturer = enumerator.CreateCapturer(deviceName, null);
+                    IVideoCapturer capturer = enumerator.CreateCapturer(deviceName, null);
                     
-                    if (videoCapturer != null)
+                    if (capturer != null)
                     {
-                        return videoCapturer;
+                        return capturer;
                     }
                 }
             }
@@ -360,36 +279,26 @@ namespace Xamarin_Android.Droid
         {
             FrameLayout remoteVideoLayout = FindViewById<FrameLayout>(Resource.Id.remote_video_layout);
             FrameLayout localVideoLayout = FindViewById<FrameLayout>(Resource.Id.local_video_layout);
-
             //remoteVideoLayout.setPosition(REMOTE_X, REMOTE_Y, REMOTE_WIDTH, REMOTE_HEIGHT);
             //remoteRenderView.SetScalingType(SCALE_ASPECT_FILL);
-            //remoteRenderView.SetMirror(false);
+            remoteRenderView.SetMirror(false);
 
-            //if (iceConnected)
-            //{
-            //    //localVideoLayout.setPosition(
-            //    //        LOCAL_X_CONNECTED, LOCAL_Y_CONNECTED, LOCAL_WIDTH_CONNECTED, LOCAL_HEIGHT_CONNECTED);
-            //    localRenderView.SetScalingType(SCALE_ASPECT_FIT);
-            //}
-            //else
-            //{
-            //    //localVideoLayout.setPosition(
-            //    //        LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING, LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING);
-            //    localRenderView.SetScalingType(SCALE_ASPECT_FILL);
-            //}
-            //localRenderView.SetMirror(true);
+            /*if (iceConnected)
+            {
+                //localVideoLayout.setPosition(
+                //        LOCAL_X_CONNECTED, LOCAL_Y_CONNECTED, LOCAL_WIDTH_CONNECTED, LOCAL_HEIGHT_CONNECTED);
+                localRenderView.SetScalingType(SCALE_ASPECT_FIT);
+            }
+            else
+            {
+                //localVideoLayout.setPosition(
+                //        LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING, LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING);
+                localRenderView.SetScalingType(SCALE_ASPECT_FILL);
+            }*/
 
-            //localRenderView.RequestLayout();
-            //remoteRenderView.RequestLayout();
+            localRenderView.SetMirror(true);
+            localRenderView.RequestLayout();
+            remoteRenderView.RequestLayout();
         }
-
-        //public IVideoCapturer Create(string device_name)
-        //{
-        //    //front-facing only for now.
-        //    Camera1Enumerator cameraEnumator = new Camera1Enumerator();
-        //    IVideoCapturer videoCapture = cameraEnumator.CreateCapturer(device_name, null);
-        //    return videoCapture;
-
-        //}
     }
 }
