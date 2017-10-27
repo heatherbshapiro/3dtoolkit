4320997ec94e232c7a44483500c9bcd0dc17c5dd
Phil Cherner <phcherne@microsoft.com>
2017-10-27 11:18:56 -0400

    Removed gestureTest

--- Xamarin Android/Xamarin_Android.Android/Resources/Resource.designer.cs
+++ Xamarin Android/Xamarin_Android.Android/Resources/Resource.designer.cs
@@ -60,29 +60,23 @@ namespace Xamarin_Android.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int NameText = 2131034115;
-			
-			// aapt resource value: 0x7f050002
-			public const int URLText = 2131034114;
-			
 			// aapt resource value: 0x7f050001
-			public const int gesture_view = 2131034113;
-			
-			// aapt resource value: 0x7f050005
-			public const int main = 2131034117;
+			public const int NameText = 2131034113;
 			
 			// aapt resource value: 0x7f050000
-			public const int mainlayout = 2131034112;
+			public const int URLText = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int main = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int myButton = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int server_list = 2131034117;
 			
 			// aapt resource value: 0x7f050004
-			public const int myButton = 2131034116;
-			
-			// aapt resource value: 0x7f050007
-			public const int server_list = 2131034119;
-			
-			// aapt resource value: 0x7f050006
-			public const int textItem = 2131034118;
+			public const int textItem = 2131034116;
 			
 			static Id()
 			{
@@ -98,16 +92,13 @@ namespace Xamarin_Android.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int GestureTest = 2130903040;
+			public const int Main = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int ServerList = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int ServerList = 2130903042;
-			
-			// aapt resource value: 0x7f030003
-			public const int VideoStream = 2130903043;
+			public const int VideoStream = 2130903042;
 			
 			static Layout()
 			{
